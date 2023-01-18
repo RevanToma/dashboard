f9aa58bef6d0f003189b47e97f722e47c774af3e
RevanToma <tamrevan@gmail.com>
2023-01-18 15:00:09 +0100

    added the missing s to http

--- src/js/config.js
+++ src/js/config.js
@@ -4,7 +4,7 @@ export const RANDOM_JOKE_API =
   "https://official-joke-api.appspot.com/random_joke";
 export const UNSPLASH_ID = "MZE_fLZL0Zvz6J0aBV53gezMWFSJNHQ_4EpheW8KSF8";
 export const WEATHER_API_KEY = "f365e6e92904c6192868a84da639c28d";
-export const TIME_API_URL = "http://worldtimeapi.org/api/timezone/";
+export const TIME_API_URL = "https://worldtimeapi.org/api/timezone/";
 export const BACKGROUND_IMAGE_SEC = 25;
 export const ACTIVITY_SEC = 20;
 export const JOKE_SEC = 25;
